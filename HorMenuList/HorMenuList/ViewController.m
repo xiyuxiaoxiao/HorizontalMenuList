7eebdd12c7d44250c2c883e0f42c85e631dea825
WYJ <1468470440@qq.com>
2017-06-01 17:51:55 +0800

    ...

--- HorMenuList/HorMenuList/ViewController.m
+++ HorMenuList/HorMenuList/ViewController.m
@@ -20,13 +20,39 @@
     
     NSArray *titleArray = @[@"BT0",@"BT1",@"BT2",@"BT3",@"BT4",@"BT5",@"BT6",@"BT7",@"BT8",@"BT9"];
     
+    MenuListView *view = [[MenuListView alloc] init];
+    view.frame = CGRectMake(50, 100, 300, 40);
+    view.backgroundColor = [UIColor whiteColor];
+    [view setTarget:self action:@selector(buttonAction:)];
+    [view seletDefaultIndex:6];
+    [view setTitleArray:titleArray];
+    [self.view addSubview:view];
+    
+}
+
+//初始化2
+-(void)addView2{
+    
+    NSArray *titleArray = @[@"BT0",@"BT1",@"BT2",@"BT3",@"BT4",@"BT5",@"BT6",@"BT7",@"BT8",@"BT9"];
+    
+    MenuListView *view = [[MenuListView alloc] init];
+    view.frame = CGRectMake(50, 100, 300, 40);
+    view.backgroundColor = [UIColor whiteColor];
+    [view setTarget:self action:@selector(buttonAction:)];
+    [view seletDefaultIndex:6];
+    [view setTitleArray:titleArray];
+    [self.view addSubview:view];
+}
+
+-(void)addView1{
+    NSArray *titleArray = @[@"BT0",@"BT1",@"BT2",@"BT3",@"BT4",@"BT5",@"BT6",@"BT7",@"BT8",@"BT9"];
+    
     MenuListView *view = [[MenuListView alloc] initWithArray:titleArray];
     view.frame = CGRectMake(50, 100, 300, 40);
     view.backgroundColor = [UIColor whiteColor];
     [view setTarget:self action:@selector(buttonAction:)];
     [view seletDefaultIndex:6];
     [self.view addSubview:view];
-    
 }
 
 -(void)buttonAction:(NSString *)index {
